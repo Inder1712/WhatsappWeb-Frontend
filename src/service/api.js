dfb11e39dd69c5a019f084898ec92216f07dc9d1
Inder <inderpreet171222@gmail.com>
2023-05-31 20:42:50 +0530

    update

--- src/service/api.js
+++ src/service/api.js
@@ -1,66 +1,69 @@
-import axios from "axios"
-const url="http://localhost:8000/add"
-export const addUser=async(data)=>
-{
- try {
-   await axios.post(url,data)
-    
- } catch (error) {
-    console.log(error);
-    
- }
-}
-export const getUser=async()=>{
+import axios from "axios";
+const url = "https://whatsappweb-api.onrender.com/add";
+export const addUser = async (data) => {
   try {
-    const userData= await axios.get("http://localhost:8000/users")
+    await axios.post(url, data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getUser = async () => {
+  try {
+    const userData = await axios.get(
+      "https://whatsappweb-api.onrender.com/users"
+    );
     return userData.data;
-
   } catch (error) {
     console.log("Error in api.js");
   }
-}
-export const setConversation=async(data)=>{
+};
+export const setConversation = async (data) => {
   try {
-    await axios.post("http://localhost:8000/conversation/add",data)
-    
+    await axios.post(
+      "https://whatsappweb-api.onrender.com/conversation/add",
+      data
+    );
   } catch (error) {
     console.log("Error in setConversation in api.js");
-    
   }
-}
- 
-export const getConversation=async(data)=>{
+};
+
+export const getConversation = async (data) => {
   try {
-    let res= await axios.post("http://localhost:8000/conversation/get",data)
-    return res.data
+    let res = await axios.post(
+      "https://whatsappweb-api.onrender.com/conversation/get",
+      data
+    );
+    return res.data;
   } catch (error) {
-    console.log("Error from getConversation in api.js")
-    
+    console.log("Error from getConversation in api.js");
   }
-}
-export const newMessage=async(data)=>{
+};
+export const newMessage = async (data) => {
   try {
-    await axios.post("http://localhost:8000/message/add",data)
+    await axios.post("https://whatsappweb-api.onrender.com/message/add", data);
   } catch (error) {
-    console.log("Error from newMessage from api.js")
+    console.log("Error from newMessage from api.js");
   }
-}
-export const getMessage=async(id)=>{
+};
+export const getMessage = async (id) => {
   try {
-    const res=await axios.get(`http://localhost:8000/message/get/${id}`)
+    const res = await axios.get(
+      `https://whatsappweb-api.onrender.com/message/get/${id}`
+    );
     console.log(id);
-    return res.data
+    return res.data;
   } catch (error) {
-    console.log("Error in getMessage in api.js",error.message);
-    
+    console.log("Error in getMessage in api.js", error.message);
   }
-}
-export const uploadFile=async(data)=>{
+};
+export const uploadFile = async (data) => {
   try {
-   return await axios.post("http://localhost:8000/file/upload",data)
-    
+    return await axios.post(
+      "https://whatsappweb-api.onrender.com/file/upload",
+      data
+    );
   } catch (error) {
     console.log("Error in uploadFile in api.js");
-    
   }
-} 
+};
